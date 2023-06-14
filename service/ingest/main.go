71e5afa40d15645af7e84939b5e386127ca7cfcc
Juergen Enge <github@betalabs.ch>
2023-06-14 15:54:14 +0200

    corrections

--- service/ingest/main.go
+++ service/ingest/main.go
@@ -4,9 +4,11 @@ import (
 	"emperror.dev/errors"
 	"flag"
 	"github.com/je4/filesystem/v2/pkg/vfsrw"
+	"github.com/je4/filesystem/v2/pkg/writefs"
 	"github.com/je4/mediaserver/v2/pkg/config"
 	lm "github.com/je4/utils/v2/pkg/logger"
 	_ "github.com/lib/pq"
+	"io/fs"
 	"net"
 	"os"
 )
@@ -29,7 +31,7 @@ func main() {
 		panic(errors.Wrapf(err, "cannot unmarshal config toml data from '%s'", *configFile))
 	}
 
-	daLogger, lf := lm.CreateLogger("ocfl", string(conf.LogFile), nil, string(conf.LogLevel), LOGFORMAT)
+	daLogger, lf := lm.CreateLogger("ingest", string(conf.LogFile), nil, string(conf.LogLevel), LOGFORMAT)
 	defer lf.Close()
 
 	vfs, err := vfsrw.NewFS(conf.VFS, daLogger)
@@ -40,6 +42,20 @@ func main() {
 	if err != nil {
 		daLogger.Panicf("cannot listen to tcp %s", conf.Addr)
 	}
+
+	if err := writefs.WriteFile(vfs, "vfs:/switch_ch/testarchive/data/hello.txt", []byte("hello")); err != nil {
+		daLogger.Errorf("error writing file")
+	}
+
+	fs.WalkDir(vfs, "vfs:/switch_ch/", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			daLogger.Infof("[d] %s", path)
+		} else {
+			daLogger.Infof("[f] %s", path)
+		}
+
+		return nil
+	})
 
 	_ = vfs
 	_ = listener
